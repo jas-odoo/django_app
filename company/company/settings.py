5cacf54749dac562eec323f33aa0eccdbf2e4908
Jamin Shah <jaiminshah2009@gmail.com>
2021-10-30 19:30:50 +0530

    Allow all hosts

--- company/company/settings.py
+++ company/company/settings.py
@@ -52,6 +52,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'company.urls'
 
+ALLOWED_HOSTS = ['*']
+
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
